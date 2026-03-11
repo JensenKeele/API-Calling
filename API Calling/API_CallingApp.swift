94716b5490ddb158ac34628d774da282e7e16a1a
jkeele <jkeele@bsd220.org>
2026-03-11 10:16:15 -0500

    Initial Commit

---
+++ API Calling/API_CallingApp.swift
@@ -0,0 +1,17 @@
+//
+//  API_CallingApp.swift
+//  API Calling
+//
+//  Created by Jensen Keele on 3/11/26.
+//
+
+import SwiftUI
+
+@main
+struct API_CallingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
